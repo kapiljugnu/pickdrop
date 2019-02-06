8cccf13e31e2abee1245b7a97e3784dd8f1e4396
Kapil <kr_kapil@hotmail.com>
2019-02-06 22:40:30 +0530

    reset problem

--- src/Location/Input.js
+++ src/Location/Input.js
@@ -5,8 +5,10 @@ import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/Sta
 import './Input.css';
 
 class Input extends React.Component {
-    pickRef = React.createRef();
-    dropRef = React.createRef();
+    pickSearchBoxRef = React.createRef();
+    dropSearchBoxRef = React.createRef();
+    pickInputRef = React.createRef();
+    dropInputRef = React.createRef();
 
     static propTypes = {
         onSubmit: PropTypes.func.isRequired,
@@ -18,12 +20,12 @@ class Input extends React.Component {
     }
 
     onPickChange = () => {
-        const input = this.pickRef.current.getPlaces()[0].formatted_address;
+        const input = this.pickSearchBoxRef.current.getPlaces()[0].formatted_address;
         this.setState({ pickLocation: input });
     }
 
     onDropChange = () => {
-        const input = this.dropRef.current.getPlaces()[0].formatted_address;
+        const input = this.dropSearchBoxRef.current.getPlaces()[0].formatted_address;
         this.setState({ dropLocation: input });
     }
 
@@ -33,17 +35,19 @@ class Input extends React.Component {
     }
 
     onReset = () => {
+        this.pickInputRef.current.value = '';
+        this.dropInputRef.current.value = '';
         this.setState({ pickLocation: '', dropLocation: '' })
     }
 
     render() {
         return (
             <form>
-                <StandaloneSearchBox ref={this.pickRef} onPlacesChanged={this.onPickChange}>
-                    <input type="text" id="pick" placeholder="Pickup Location"></input>
+                <StandaloneSearchBox ref={this.pickSearchBoxRef} onPlacesChanged={this.onPickChange}>
+                    <input type="text" id="pick" ref={this.pickInputRef} placeholder="Pickup Location"></input>
                 </StandaloneSearchBox>
-                <StandaloneSearchBox ref={this.dropRef} onPlacesChanged={this.onDropChange}>
-                    <input type="text" id="drop" placeholder="Where to?"></input>
+                <StandaloneSearchBox ref={this.dropSearchBoxRef} onPlacesChanged={this.onDropChange}>
+                    <input type="text" id="drop" ref={this.dropInputRef} placeholder="Where to?"></input>
                 </StandaloneSearchBox>
                 <input type="submit" onClick={this.onFormSubmit}></input>
                 <input type="button" value="Reset" onClick={this.onReset}></input>

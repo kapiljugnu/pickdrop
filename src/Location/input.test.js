5fa905b77f7759df5b44d4bb25f1298c94dc397e
Kapil <kr_kapil@hotmail.com>
2019-02-07 12:26:54 +0530

    input test case changes

--- src/Location/input.test.js
+++ src/Location/input.test.js
@@ -1,39 +1,42 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import Input from './Input';
 
+jest.mock('react-dom');
 
 describe('Input', () => {
+
+    const querySelector = jest.fn();
+    ReactDom.findDOMNode = jest.fn(() => ({ querySelector }));
+    const onSubmit = jest.fn();
+
+    beforeAll(() => {
+        global.google = { maps: { places: { SearchBox: class { } }, event: { addListener: jest.fn(), removeListener: jest.fn() } } };
+    });
+
     it('should match the snapshot', () => {
-        const inputTree = renderer.create(<Input onSubmit={() => { }} />).toJSON();
+        const inputTree = renderer.create(<Input onSubmit={onSubmit} />).toJSON();
         expect(inputTree).toMatchSnapshot();
-    })
+    });
 
     it('onSubmit should be called with pick and drop location', () => {
-        const onSubmit = jest.fn();
         const LOCATION_PICK = 'location pick';
         const LOCATION_DROP = 'location drop';
-        const InputComponent = mount(<Input onSubmit={onSubmit} />);
-        const form = InputComponent.find('form');
-        form.find('#pick').simulate('change', { target: { value: LOCATION_PICK } });
-        form.find('#drop').simulate('change', { target: { value: LOCATION_DROP } });
-        form.find('input[type="submit"]').simulate('click');
+        const input = mount(<Input onSubmit={onSubmit} />)
+            .setState({ pickLocation: LOCATION_PICK, dropLocation: LOCATION_DROP });
+        input.find('form').find('input[type="submit"]').simulate('click');
         expect(onSubmit).toHaveBeenCalledWith(LOCATION_PICK, LOCATION_DROP);
     });
 
     it('form should be reset on reset click', () => {
-        const onSubmit = jest.fn();
         const LOCATION_PICK = 'location pick';
         const LOCATION_DROP = 'location drop';
-        const InputComponent = mount(<Input onSubmit={onSubmit} />);
-        const form = InputComponent.find('form');
-        const pick = form.find('#pick');
-        const drop = form.find('#drop');
-        pick.simulate('change', { target: { value: LOCATION_PICK } });
-        drop.simulate('change', { target: { value: LOCATION_DROP } });
-        form.find('input[type="button"]').simulate('click');
-        expect(pick.props().value).toBe('');
-        expect(drop.props().value).toBe('');
+        const input = mount(<Input onSubmit={onSubmit} />)
+            .setState({ pickLocation: LOCATION_PICK, dropLocation: LOCATION_DROP });
+        input.find('form').find('input[type="button"]').simulate('click');
+        expect(input.state().pickLocation).toBe('');
+        expect(input.state().dropLocation).toBe('');
     });
 });

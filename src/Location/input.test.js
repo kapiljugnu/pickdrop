97a1e05f1e6e64750ae0ab6bf2d53de755ce60c4
Kapil <kr_kapil@hotmail.com>
2019-02-07 12:28:00 +0530

    refactor input test case

--- src/Location/input.test.js
+++ src/Location/input.test.js
@@ -11,6 +11,8 @@ describe('Input', () => {
     const querySelector = jest.fn();
     ReactDom.findDOMNode = jest.fn(() => ({ querySelector }));
     const onSubmit = jest.fn();
+    const LOCATION_PICK = 'location pick';
+    const LOCATION_DROP = 'location drop';
 
     beforeAll(() => {
         global.google = { maps: { places: { SearchBox: class { } }, event: { addListener: jest.fn(), removeListener: jest.fn() } } };
@@ -22,8 +24,6 @@ describe('Input', () => {
     });
 
     it('onSubmit should be called with pick and drop location', () => {
-        const LOCATION_PICK = 'location pick';
-        const LOCATION_DROP = 'location drop';
         const input = mount(<Input onSubmit={onSubmit} />)
             .setState({ pickLocation: LOCATION_PICK, dropLocation: LOCATION_DROP });
         input.find('form').find('input[type="submit"]').simulate('click');
@@ -31,8 +31,6 @@ describe('Input', () => {
     });
 
     it('form should be reset on reset click', () => {
-        const LOCATION_PICK = 'location pick';
-        const LOCATION_DROP = 'location drop';
         const input = mount(<Input onSubmit={onSubmit} />)
             .setState({ pickLocation: LOCATION_PICK, dropLocation: LOCATION_DROP });
         input.find('form').find('input[type="button"]').simulate('click');

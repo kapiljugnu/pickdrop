181a7755462cd5b9883daaf6bb87ab4f51ff1d6d
Kapil <kr_kapil@hotmail.com>
2019-02-08 16:16:47 +0530

    unit testing update

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import App from './index';
+import App, { App as ScriptChild } from './index';
+import { SERVER_ERROR, INPUT_ERROR } from '../../constants/messages';
 
-it('renders without crashing', () => {
-  const app = mount(<App
-    loadingElement={<div style={{ height: `100%` }} />}
-    googleMapURL='googleMapURL'
-  />).setState({ loadingState: 'LOADED' });
-  expect(toJson(app)).toMatchSnapshot();
+
+describe('App.js', () => {
+
+  const loadingElement = <div style={{ height: `100%` }} />
+  const googleMapURL = 'googleMapURL';
+
+  it('renders without crashing', () => {
+    const app = mount(<App
+      loadingElement={loadingElement}
+      googleMapURL={googleMapURL}
+    />).setState({ loadingState: 'LOADED' });
+    expect(toJson(app)).toMatchSnapshot();
+  });
+
+  it('onReset the app', () => {
+    const appTree = mount(<App loadingElement={loadingElement}
+      googleMapURL={googleMapURL} />).setState({ loadingState: 'LOADED' });
+    const app = appTree.find(ScriptChild).setState({ hasError: true, message: 'mock' });
+    expect(app.state().message).toBe('mock');
+    expect(app.state().hasError).toBeTruthy();
+    app.instance().onReset();
+    expect(app.state().hasError).toBeFalsy();
+    expect(app.state().message).toBe('');
+  });
+
+  it('onSubmit the location with no parameter', () => {
+    const appTree = mount(<App loadingElement={loadingElement}
+      googleMapURL={googleMapURL} />).setState({ loadingState: 'LOADED' });
+    const app = appTree.find(ScriptChild);
+    app.instance().onLocationSubmit();
+    expect(app.state().hasError).toBeTruthy();
+    expect(app.state().message).toBe(INPUT_ERROR);
+  });
+
+  it('onSubmit the location with parameter', () => {
+    const appTree = mount(<App loadingElement={loadingElement}
+      googleMapURL={googleMapURL} />).setState({ loadingState: 'LOADED' });
+    const app = appTree.find(ScriptChild);
+    app.instance().onLocationSubmit('location pick', 'location drop');
+    expect(app.state().hasError).toBeFalsy();
+    expect(app.state().message).toBe('');
+    expect(app.state().isFetching).toBeTruthy();
+    process.nextTick(() => {
+      expect(app.state().isFetching).toBeFalsy();
+      expect(app.state().path).toEqual([1, 2])
+    });
+  });
 });

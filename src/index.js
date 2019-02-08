d6f08052668432b0a0bb0429f670bd433b507da2
Kapil <kr_kapil@hotmail.com>
2019-02-08 14:57:15 +0530

    ref change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 const googleMapUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API}&v=3.exp&libraries=geometry,drawing,places`;
 
